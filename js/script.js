12555a538c0ce80d9a987f889917b6719e7f1a49
AlexandreStang <lealexandrestang@gmail.com>
2023-02-04 11:07:29 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+const colors = {pos: "green-txt", mid: "yellow-txt", neg: "red-txt"}
+
+document.addEventListener('DOMContentLoaded', function () {
+
+
+    // SET MAX RELEASE YEAR - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    document.getElementById("min-year-input").setAttribute("max", new Date().getFullYear());
+
+    // INTERACTIVE SCORE SLIDER - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    // Variables
+    const scorePercent = document.getElementById("min-score-percentage");
+    const scoreSlider = document.getElementById("min-score-input");
+
+    // Set score percentage and color based on the default score
+    const defaultScore = scoreSlider.value;
+    scorePercent.innerHTML = defaultScore + "%";
+    scorePercent.classList.add(getColor(defaultScore));
+
+    // Change score percentage and color based on the new value
+    scoreSlider.addEventListener("input", function (e) {
+
+        var score = this.value
+        scorePercent.innerHTML = score + "%";
+
+        // Remove all color classes
+        scorePercent.classList.remove(colors.pos);
+        scorePercent.classList.remove(colors.mid);
+        scorePercent.classList.remove(colors.neg);
+
+        // Add the appropriate color class
+        scorePercent.classList.add(getColor(score));
+
+    });
+
+});
+
+// Return the appropriate color class based on the score percentage
+function getColor(score) {
+
+    if (score > 69) {
+        return colors.pos;
+    } else if (score > 39) {
+        return colors.mid;
+    } else {
+        return colors.neg;
+    }
+
+}
+
