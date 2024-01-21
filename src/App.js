79a323fe0efdb8073adff560b082156fedd3e705
AlexandreStang <91079041+AlexandreStang@users.noreply.github.com>
2024-01-21 17:06:18 -0500

    Form now properly gets a movie's ID and the movie's details

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import './css/styles.css';
 import Logo from './img/logo/randomovie.svg';
 import './config.js';
-import PopUp from "./PopUp";
+//import PopUp from "./PopUp";
 
 // IMAGES
 const MINOR_POSTER_WIDTH = "w300";
@@ -29,25 +29,25 @@ const App = () => {
 
     //const [active, setActive] = useState("");
 
-    // STATES
+    // STATES - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     const [bannerPath, setBannerPath] = useState([]);
 
     const [genres, setGenres] = useState([]);
     const [providers, setProviders] = useState([]);
 
-    const [randomPageNumber, setRandomPageNumber] = useState([]);
-    const [randomMovie, setRandomMovie] = useState([]);
+    const [movieDetails, setMovieDetails] = useState([]);
+    const [movieCredits, setMovieCredits] = useState([]);
+    const [movieReleaseDates, setMovieReleaseDates] = useState([]);
 
     const [trendingFilms, setTrendingFilms] = useState([]);
 
-    const [rmGenre, setRmGenre] = useState(28);
-    const [rmReleaseYear, setRmReleaseYear] = useState(minYear);
-    const [rmWatchProvider, setRmWatchProvider] = useState(8);
-    const [rmMinScore, setRmMinScore] = useState(defaultMinScore);
+    const [formGenre, setFormGenre] = useState(28);
+    const [formReleaseYear, setFormReleaseYear] = useState(minYear);
+    const [formWatchProvider, setFormWatchProvider] = useState(8);
+    const [formMinScore, setFormMinScore] = useState(defaultMinScore);
 
 
-    // FUNCTIONS
-
+    // GET METHODS - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     const getBanner = async () => {
         const response = await fetch(global.config.API.URL + "trending/movie/" + timeWindowWeek + global.config.API.KEY);
         const data = await response.json();
@@ -78,12 +78,11 @@ const App = () => {
     }
 
 
-    // TODO: Need to completed/renamed/moved elsewhere
     const getRandomPage = async () => {
         let response = await fetch(global.config.API.URL + "discover/movie" + global.config.API.KEY +
             global.config.LANGUAGE + "&region=" + global.config.REGION +
-            "&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&" + rmReleaseYear +
-            "&vote_average.gte=" + rmMinScore/10 + "&with_genres=" + rmGenre + "&with_watch_providers=" + rmWatchProvider +
+            "&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&" + formReleaseYear +
+            "&vote_average.gte=" + formMinScore/10 + "&with_genres=" + formGenre + "&with_watch_providers=" + formWatchProvider +
             global.config.REGION + "&with_watch_monetization_types=flatrate");
         const data = await response.json();
 
@@ -91,29 +90,34 @@ const App = () => {
     }
 
 
-    // TODO: Need to completed/renamed/moved elsewhere
-    const getRandomMovie = async () => {
+    const getRandomMovieID = async () => {
         const pageNumber = await getRandomPage();
 
         const response = await fetch(global.config.API.URL + "discover/movie" + global.config.API.KEY +
             global.config.LANGUAGE + "&region=" + global.config.REGION +
-            "&sort_by=popularity.desc&include_adult=false&include_video=false&page=" + pageNumber + "&" + rmReleaseYear +
-            "&vote_average.gte=" + rmMinScore/10 + "&with_genres=" + rmGenre + "&with_watch_providers=" + rmWatchProvider +
+            "&sort_by=popularity.desc&include_adult=false&include_video=false&page=" + pageNumber + "&" + formReleaseYear +
+            "&vote_average.gte=" + formMinScore/10 + "&with_genres=" + formGenre + "&with_watch_providers=" + formWatchProvider +
             global.config.REGION + "&with_watch_monetization_types=flatrate");
         const data = await response.json();
 
         // TODO: DEBUG CONSOLE
-        console.log(rmGenre);
-        console.log(rmReleaseYear);
-        console.log(rmWatchProvider);
-        console.log(rmMinScore);
+        // console.log(formGenre);
+        // console.log(formReleaseYear);
+        // console.log(formWatchProvider);
+        // console.log(formMinScore);
 
-        console.log(data.results);
-        return data.results[Math.floor(Math.random() * data.results.length)];
+        //console.log(data.results);
+        return data.results[Math.floor(Math.random() * data.results.length)].id;
     }
 
+    const getMovieDetails = async (movieID) => {
+        const response = await fetch(global.config.API.URL + "movie/" + movieID + global.config.API.KEY + global.config.LANGUAGE)
+        const data = await response.json();
 
-    const getTrendingFilms = async (timeWindow) => {
+        return data;
+    }
+
+    const getTrendingMovies = async (timeWindow) => {
         const response = await fetch(global.config.API.URL + "trending/movie/" + timeWindow +
             global.config.API.KEY + global.config.LANGUAGE);
         const data = await response.json();
@@ -122,31 +126,119 @@ const App = () => {
     }
 
 
+    // OTHER METHODS
     function updateTrendingFilms(timeWindow) {
-        getTrendingFilms(timeWindow).then(data => setTrendingFilms(data));
+        trendingTimeWindow = timeWindow;
+        getTrendingMovies(timeWindow).then(data => setTrendingFilms(data));
     }
 
 
     function handleSubmit() {
-        getRandomPage().then(data => setRandomPageNumber((data)));
-        getRandomMovie(randomPageNumber).then(data => setRandomMovie(data));
+
+        getRandomMovieID().then(id => {
+            getMovieDetails(id).then(data => setMovieDetails(data));
+        })
+
+
+    }
+
+    function closePopup() {
+        setMovieDetails([]);
     }
 
 
-    // EFFECTS
+    // POPUP COMPONENT - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    function PopUp ({movie}) {
+
+        return (
+            <div className="popup-container" id="popup-container">
+                <div className="popup light-bg">
+                    <header>
+                        <a href="#" onClick={handleSubmit}>Not what you’re looking for? Click here for another random movie!</a>
+                        <a href="#" className="right-side-icon" onClick={closePopup}><i className="fas fa-times icon"></i></a>
+                    </header>
+                    <div className="flex-container">
+                        <aside>
+                            <div className="separator"><img src="img/posters/spider-man.jpg" alt="Avatar Poster" className="poster"></img></div>
+                            <h4 className="separator">Available for streaming on Netflix</h4>
+                            <h4 className="separator"><a href=""><i className="fas fa-link left-side-icon"></i> Watch Trailer</a></h4>
+                        </aside>
+                        <div className="popup-text">
+                            <hgroup className="green-separator">
+                                <h1>{movie.title}</h1>
+                                <p>2021 • PG-13 • Action, Adventure, Science Fiction
+                                    • 2h 28m</p>
+                            </hgroup>
+                            <div className="separator">
+                                <div className="tagline-score">
+                                    <h4>The Multiverse unleashed.</h4>
+                                    <h4>User Score: <span className="green-txt" id="popup-score">84%</span></h4>
+                                </div>
+                                <p>
+                                    Peter Parker is unmasked and no longer able to separate his normal life from the high-stakes of
+                                    being a
+                                    super-hero. When he asks for help from Doctor Strange the stakes become even more dangerous,
+                                    forcing him
+                                    to discover what it truly means to be Spider-Man.
+                                </p>
+                            </div>
+                            <div className="people">
+                                <ul>
+                                    <li><h4>Director</h4></li>
+                                    <li>Jon Watts</li>
+                                </ul>
+                                <ul>
+                                    <li><h4>Characters</h4></li>
+                                    <li>Stan Lee</li>
+                                </ul>
+                                <ul>
+                                    <li><h4>Characters</h4></li>
+                                    <li>Steve Ditko</li>
+                                </ul>
+                                <ul>
+                                    <li><h4>Writer</h4></li>
+                                    <li>Chris McKenna</li>
+                                </ul>
+                                <ul>
+                                    <li><h4>Writer</h4></li>
+                                    <li>Erik Sommers</li>
+                                </ul>
+                                <ul className="casting">
+                                    <li><h4>Top Casting</h4></li>
+                                    <li>Tom Holland</li>
+                                    <li>Zendaya</li>
+                                    <li>Benedict Cumberbatch</li>
+                                    <li>Jacob Batalon</li>
+                                    <li>Jon Favreau</li>
+                                    <li>Jamie Foxx</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="popup-bg-overlay"></div>
+            </div>
+        )
+
+    }
+
+
+    // EFFECTS - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     useEffect(() => {
         getBanner().then(data => setBannerPath(data));
         getGenres().then(data => setGenres(data));
         getProviders().then(data => setProviders(data));
-        getTrendingFilms(trendingTimeWindow).then(data => setTrendingFilms(data));
+        getTrendingMovies(trendingTimeWindow).then(data => setTrendingFilms(data));
     }, [])
 
 
-    // INDEX.HTML
+
+    // INDEX.HTML - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     return (
 
         <div>
-
+            {movieDetails?.length !== 0 ? <PopUp movie={movieDetails} /> : ""}
 
             <section className="hero dark-bg">
                 <div className="wrapper">
@@ -160,13 +252,13 @@ const App = () => {
                             <p>Tell us what you're looking for and receive something at random!</p>
                         </hgroup>
 
-                        <form id="random-movie-form" onSubmit={handleSubmit}>
+                        <form id="random-movie-form">
 
                             <div className="form-grid">
                                 <div className="form-item">
                                     <label htmlFor="genre-select">Genre</label>
                                     <div className="select-container">
-                                        <select name="genre" id="genre-select" value={rmGenre} onChange={(e) => setRmGenre(e.target.value)}>
+                                        <select name="genre" id="genre-select" value={formGenre} onChange={(e) => setFormGenre(e.target.value)}>
                                             {genres.map((genre) => (<option value={genre.id}>{genre.name}</option>))}
                                         </select>
                                     </div>
@@ -177,14 +269,14 @@ const App = () => {
                                     <input type="number" min={minYear} max={new Date().getFullYear()}
                                            step="1" name="min-year"
                                            id="min-year-input"
-                                           value={rmReleaseYear}
-                                           onChange={(e) => setRmReleaseYear(e.target.value)}/>
+                                           value={formReleaseYear}
+                                           onChange={(e) => setFormReleaseYear(e.target.value)}/>
                                 </div>
 
                                 <div className="form-item">
                                     <label htmlFor="streaming-select">Watch Provider</label>
                                     <div className="select-container">
-                                        <select name="streaming" id="streaming-select" value={rmWatchProvider} onChange={(e) => setRmWatchProvider(e.target.value)}>
+                                        <select name="streaming" id="streaming-select" value={formWatchProvider} onChange={(e) => setFormWatchProvider(e.target.value)}>
                                             {providers.slice(0, maxProviders).map((provider) => (<option
                                                 value={provider.provider_id}>{provider.provider_name}</option>))}
                                         </select>
@@ -194,14 +286,14 @@ const App = () => {
                                 <div className="form-item">
                                     <div className="range-label"><label htmlFor="min-score-input">Min. User
                                         Score</label><span
-                                        className={"min-score-percentage " + (rmMinScore > 69 ? "green-txt" :
-                                            (rmMinScore > 39 ? "yellow-txt" : "red-txt"))}
-                                        id="min-score-percentage">{rmMinScore + "%"}</span></div>
-                                    <input type="range" min="0" max="100" step="1" placeholder={rmMinScore}
+                                        className={"min-score-percentage " + (formMinScore > 69 ? "green-txt" :
+                                            (formMinScore > 39 ? "yellow-txt" : "red-txt"))}
+                                        id="min-score-percentage">{formMinScore + "%"}</span></div>
+                                    <input type="range" min="0" max="100" step="1" placeholder={formMinScore}
                                            name="min-score"
                                            id="min-score-input"
-                                           value={rmMinScore}
-                                           onChange={(e) => setRmMinScore(e.target.value)}/>
+                                           value={formMinScore}
+                                           onChange={(e) => setFormMinScore(e.target.value)}/>
                                 </div>
 
                                 <div className="form-item checkbox-container">
@@ -213,7 +305,7 @@ const App = () => {
                         </form>
 
                         <div className="button-container">
-                            <button id="random-movie-submit" onClick={() => getRandomMovie()}>Search</button>
+                            <button id="random-movie-submit" onClick={handleSubmit}>Search</button>
                         </div>
 
                     </div>
