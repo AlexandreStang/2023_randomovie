4ac701cd32e61ffb897a7a347928992de41f0f92
AlexandreStang <91079041+AlexandreStang@users.noreply.github.com>
2025-01-16 14:43:18 -0500

    Added a custom id generator for the select input component

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -2,12 +2,15 @@ import React from "react";
 
 export default function Select({data, config, onChangeOption}) {
     const {title, value, name} = config
+    const custom_id = title.toLowerCase().replace(/\s+/g, '-')
 
     return (
         <div className="form-item">
-            <label htmlFor="streaming-select">{title}</label>
+            <label htmlFor={custom_id}>{title}</label>
             <div className="select-container">
-                <select onChange={(e) => onChangeOption(e.target.value)}>
+                <select
+                    onChange={(e) => onChangeOption(e.target.value)}
+                    id={custom_id}>
                     <option value={null}>{""}</option>
                     {data.map((item) => (<option value={item[value]}>{item[name]}</option>))}
                 </select>
