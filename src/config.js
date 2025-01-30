d569c5ebd550fe8b333c396b2d657f3ea8bfa2ef
AlexandreStang <91079041+AlexandreStang@users.noreply.github.com>
2025-01-30 14:00:57 -0500

    Made the API key into an environment variable

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = global.config = {
     API: {
         URL : "https://api.themoviedb.org/3/",
-        KEY : "?api_key=f4f9e1b6b219580330ca8cc48f5b1165",
+        KEY : process.env.REACT_APP_API_KEY,
         IMAGE_URL : "https://image.tmdb.org/t/p/",
         IMAGE_WIDTH : {
             SMALL_POSTER: "w300",
@@ -11,8 +11,7 @@ module.exports = global.config = {
         TIME_WINDOW : {
             DAY: "day",
             WEEK: "week"
-        }
-        ,
+        },
         MAX_PAGES: 500
     },
     LANGUAGE : "en-US",
