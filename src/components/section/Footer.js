ece21ec969ee338951ad6b5c6625546ad34e80fa
AlexandreStang <91079041+AlexandreStang@users.noreply.github.com>
2025-02-02 20:18:46 -0500

    Updated my portfolio URL

--- src/components/section/Footer.js
+++ src/components/section/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function footer() {
     return (
         <footer>
-            <p>Built by <a href="https://alexandrestang.github.io/">Alexandre Stang</a> | Powered by <a
+            <p>Built by <a href="https://alexandrestang.com/">Alexandre Stang</a> | Powered by <a
                 href="https://www.themoviedb.org/">TheMovieDB</a></p>
         </footer>
     )
